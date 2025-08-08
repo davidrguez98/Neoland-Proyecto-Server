a3b14108fc064d132661577ffa18750d4e19ef1a
davidrguez98 <ropeda98@gmail.com>
2025-08-08 13:51:24 +0200

    dashboard - new product ok

--- public/javascripts/dashboard.page.js
+++ public/javascripts/dashboard.page.js
@@ -1,13 +1,12 @@
 function main() {
     
-    const editBtn = document.querySelectorAll("#btn-edit")
-    const deleteBtn = document.querySelectorAll("#btn-delete")
+    const editBtn = document.querySelectorAll(".btn-edit")
+    const deleteBtn = document.querySelectorAll(".btn-delete")
     const newProductForm = document.querySelector("#product-form")
 
     if (!editBtn || !deleteBtn || !newProductForm) return null
     
     newProductForm.addEventListener("submit", async () => {
-        // event.preventDefault()
 
         const name = document.querySelector("#new-name").value
         const price = document.querySelector("#new-price").value
@@ -35,6 +34,34 @@ function main() {
         } catch (error) {
             console.log(error)
         }
+    })
+
+    deleteBtn.forEach(btn => {
+        btn.addEventListener("click", async (event) => {
+        
+        const id = event.target.getAttribute('data-id')
+
+        if(!id) return null
+
+        if(confirm('¿Seguro que quieres eliminar el producto?')) {
+            try {
+                const response = await fetch(`/api/products/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                })
+
+                if (response.ok) {
+                    alert('Producto eliminado correctamente.')
+                    location.reload()
+                }
+            } catch (error) {
+                console.log(error)
+                alert('Error al eliminar el producto')
+            }
+        }
+    })
     })
 }
 
